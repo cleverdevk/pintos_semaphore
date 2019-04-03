06a47ea23830a860c96e10bd5ef1b493e5a582b9
Nick Kang <cleverdevk@gmail.com>
2019-04-04 00:04:05 +0900

    First Commit

---
+++ projects/factorii/factorii.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "threads/init.h"
+#include "threads/malloc.h"
+#include "threads/synch.h"
+#include "threads/thread.h"
+
+#include "devices/timer.h"
+
+
+void run_factorii(char **argv)
+{
+        printf("implement factorii !\n");
+}
